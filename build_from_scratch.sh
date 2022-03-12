3d5acf6e338d8863817f61105863b59b76a0498e
unrealf1 <fyodor.bukreev@yandex.ru>
2022-03-12 13:08:51 +0300

    updated build scripts

--- build_from_scratch.sh
+++ build_from_scratch.sh
@@ -3,14 +3,21 @@
 echo "Building the project..."
 
 if [ ! -d "./build" ]; then
-    ./build_from_scratch.sh $@
+	mkdir ./build
 fi
 
 cd build
 
-num_procs=$(nproc)
-echo "building using ${num_procs} workers"
-if make -j${num_procs}; then
+if cmake ../ $@; then
+echo "calling make...";
+else 
+echo "CMake failed!";
+cd ..
+exit 1 
+fi
+
+cd ..
+if ./build.sh; then
 echo "build ready";
 else 
 echo "build failed!";
